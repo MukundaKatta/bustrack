912a160d7088bd6a491575ae72edadcc8647ab82
mukunda katta <mukunda.vjcs6@gmail.com>
2026-05-16 14:39:47 -0700

    fix: escape login page apostrophe

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -100,7 +100,7 @@ export default function LoginPage() {
         </form>
 
         <p className="text-center mt-4 text-sm text-gray-600">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <Link href="/signup" className="text-blue-600 hover:underline">
             Sign up
           </Link>
